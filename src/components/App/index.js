90321fc6653eb34bb0a1373983db928b106e414b
max b <maxwellbenton@gmail.com>
2021-03-16 19:31:44 -0400

    move theme content

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,37 +5,22 @@ import LoadingMessage from '../LoadingMessage/';
 import LoadMore from '../LoadMore/';
 import ResultsContainer from '../ResultsContainer/';
 import DarkModeToggle from '../DarkModeToggle';
+import { determineTheme } from '../../theme/index'
 import './App.css';
 
 
-const THEME = {
-  light: {
-    background: '#eee',
-    color: '#000'
-  },
-  dark: {
-    background: '#333',
-    color: '#eee'
-  }
-}
-
 function App() {
-
-  function determineTheme(data) {
-    return data.darkMode ? THEME.dark : THEME.light
-  }
-
   return (
       <ConfigProvider>
         <Consumer>
-          {data => (
-            <div className="App" style={determineTheme(data)}>
+          {({darkMode, setData, setUser, repositories, loading, getRepos, toggleDarkMode}) => (
+            <div className="App" style={determineTheme(darkMode)}>
               <Title />
-              <SearchForm setData={data.setData} setUser={data.setUser}/>
-              <ResultsContainer repositories={data.repositories} />
-              <LoadingMessage loading={data.loading}/>
-              <LoadMore repositories={data.repositories} getRepos={data.getRepos} />
-              <DarkModeToggle darkMode={data.darkMode} toggleDarkMode={data.toggleDarkMode} />
+              <SearchForm setData={setData} setUser={setUser}/>
+              <ResultsContainer repositories={repositories} darkMode={darkMode} />
+              <LoadingMessage loading={loading}/>
+              <LoadMore repositories={repositories} getRepos={getRepos} />
+              <DarkModeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
             </div>)
           }
         </Consumer>
