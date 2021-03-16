cbb10f7e03f6f4de03bef58a524df2174961dfa1
max b <maxwellbenton@gmail.com>
2021-03-16 19:31:18 -0400

    update RepoCard structure

--- src/components/RepoCard/index.js
+++ src/components/RepoCard/index.js
@@ -1,11 +1,21 @@
+import { determineTheme2 } from '../../theme';
 import './RepoCard.css';
 
-const RepoCard = ({node}) => {
-  console.log(node)
+const RepoCard = ({node, darkMode}) => {
+  
   return (
-    <div className="repositoryCard">
-      <p className="repoTitle">{node.name}</p>
-      <p className="repoDesc">{node.description}</p>
+    <div className="repositoryCard" style={determineTheme2(darkMode)}>
+      <section className="left">
+        <a href={node.url}><p className="repoTitle">{node.name}</p></a>
+        <p className="repoDesc">{node.description}</p>
+        <div className="languages">{node.languages.nodes.map(lang => <span key={lang.name}>{lang.name}</span>)}</div>
+      </section>
+      <section className="right">
+        <div>Forks: {node.forkCount}</div>
+        {node.isPrivate ? <div>Private</div> : <div>Public</div>}
+        <div>Open Issues: {node.issues.nodes.length} </div>
+        
+      </section>
     </div>
   )
 }
