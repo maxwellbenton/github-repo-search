751b7db3336f6dd267a246fd2f45b2938f59dcdf
max b <maxwellbenton@gmail.com>
2021-03-16 19:31:29 -0400

    add ...

--- src/components/LoadingMessage/index.js
+++ src/components/LoadingMessage/index.js
@@ -1,3 +1,3 @@
-const LoadingMessage = ({loading}) => loading ? <div id="loading">Loading</div> : null
+const LoadingMessage = ({loading}) => loading ? <div id="loading">Loading...</div> : null
 
 export default LoadingMessage
