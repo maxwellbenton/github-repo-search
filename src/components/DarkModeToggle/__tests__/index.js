b5a1135a9bc653133b6dab3cf89598e47c71e4ae
max b <maxwellbenton@gmail.com>
2021-03-17 20:59:40 -0400

    add LoadingMessage tests

---
+++ src/components/DarkModeToggle/__tests__/index.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import LoadingMessage from '../index';
+
+
+test('renders a loading message when retrieving data', async () => {
+  const data = {loading: true};
+
+  render(<LoadingMessage {...data}/>);
+  const element = await screen.findByTestId('loading');
+  expect(element).toBeInTheDocument();
+});
+
+test('does not render a loading message when not loading', () => {
+  const data = {loading: false};
+
+  render(<LoadingMessage {...data}/>);
+  const element = screen.queryByText('Loading');
+  expect(element).toBeNull();
+});
